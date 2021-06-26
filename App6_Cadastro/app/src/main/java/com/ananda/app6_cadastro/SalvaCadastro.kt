7aa73da1cdde0a475f067f718a47ae5cea79f883
AnandaHike <nandabrunetti@hotmail.com>
2021-06-26 15:25:51 -0300

    Cadastro com Foto

---
+++ App6_Cadastro/app/src/main/java/com/ananda/app6_cadastro/SalvaCadastro.kt
@@ -0,0 +1,11 @@
+package com.ananda.app6_cadastro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SalvaCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_salva_cadastro)
+    }
+}
